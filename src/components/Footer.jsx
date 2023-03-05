ad164179c187c0767460afb9052924985b23078b
Shivasharma17 <shivathink17@gmail.com>
2023-03-05 14:05:26 +0530

    added coffee icon and some animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,15 +7,32 @@ import { IconContext } from "react-icons";
 export default function Footer() {
   return (
     <div className="w-full mt-4 text-center flex flex-col">
-      <p className="flex items-center w-full justify-center gap-1">
+      <p className="flex items-center w-full justify-center gap-2">
         {" "}
-        Made with <img src={logo} alt="" width={20} />{" "}
-        <img src={tailwind} alt="" width={20} />
+        Made with{" "}
+        <a
+          href="http://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:animate-spin-slow"
+        >
+          {" "}
+          <img src={logo} alt="" width={20} />
+        </a>{" "}
+        <a
+          href="http://tailwindcss.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:animate-bounce"
+        >
+          {" "}
+          <img src={tailwind} alt="" width={20} />
+        </a>
         and
-        <span className="text-rose-500">&#x2764;</span> by{" "}
+        <span className="text-[#ba662a] text-xl">&#x2615;</span> by{" "}
         <a
           href="https://shivakodes.me"
-          className="no-underline hover:text-lime-400 flex items-center gap-1 group"
+          className="no-underline hover:text-lime-400 flex items-center gap-1 group "
         >
           {" "}
           Shiva{" "}
